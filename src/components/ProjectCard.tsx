fc3b1f44f7095cca3fb75171c25d766127187abf
pviethung <pv2hung97@gmail.com>
2022-10-31 22:59:09 +0700

    do stuff

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -14,7 +14,7 @@ const ProjectCard = ({
   code: string;
 }) => {
   return (
-    <div className=" group flex flex-col justify-between rounded-md border-2 border-stone-900 dark:border-white  flex-1 dark:text-white relative">
+    <div className="overflow-hidden group flex flex-col justify-between rounded-md border-2 border-stone-900 dark:border-white  flex-1 dark:text-white relative">
       <img loading="lazy" src={imageSrc} alt={title} />
       {/* <p
         dangerouslySetInnerHTML={{ __html: desc }}
