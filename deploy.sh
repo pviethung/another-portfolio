a067cd9c85b74387fcd2cb858a0ed6739fa542de
pviethung <pv2hung97@gmail.com>
2022-10-31 20:15:27 +0700

    init

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,10 @@ echo > .nojekyll
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git checkout -B main
-git add -A
-git commit -m 'deploy'
+# git init
+# git checkout -B main
+# git add -A
+# git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://pviethung.github.io/another-portfolio',
+  base: '/another-portfolio/',
 });
