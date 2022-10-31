fb5ce301103d5eeea503e18c9740c1e640dd0a1c
pviethung <pv2hung97@gmail.com>
2022-10-31 20:39:29 +0700

    fix a silly mistake

--- deploy.sh
+++ deploy.sh
@@ -15,15 +15,15 @@ echo > .nojekyll
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-# git init
-# git checkout -B main
-# git add -A
-# git commit -m 'deploy'
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:pviethung/another-portfolio.git gh-pages
+git push -f git@github.com:pviethung/another-portfolio.git main:gh-pages
 
 cd -
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'eshop',
     tags: ['React', 'Next', 'Firebase'],
-    src: '/eshop-thum.png',
+    src: './eshop-thum.png',
     desc: 'An e-commerce website for selling clothes, built with next and firebase',
     code: 'https://github.com/pviethung/eshop',
     demo: 'https://eshop-gilt-zeta.vercel.app/',
@@ -10,7 +10,7 @@ export const projects = [
   {
     title: 'Code playground',
     tags: ['React', 'Esbuild', 'TypeScript'],
-    src: '/playground-thum.png',
+    src: './playground-thum.png',
     desc: 'An online code editor for quick testing purposes, it can use es6 modules for importing third party packages, built with <a href="https://create-react-app.dev/" target="_blank" href="Create React App">CRA</a>',
     code: 'https://github.com/pviethung/code-playground',
     demo: 'https://code-playground-swav.vercel.app/',
@@ -18,7 +18,7 @@ export const projects = [
   {
     title: 'Travel',
     tags: ['HTML', 'CSS', 'JavaScript'],
-    src: '/travel-thum.png',
+    src: './travel-thum.png',
     desc: 'A travel website, with some stunning effects, built using HTML, CSS and JavaScript. I cloned it from this <a href="https://www.udemy.com/course/advanced-css-and-sass/" title="Advanced CSS and Sass: Flexbox, Grid, Animations and More!" target="_blank">course</a>',
     code: 'https://github.com/pviethung/travel',
     demo: 'https://pviethung.github.io/travel/',
