6dbcf8205d4daa2532a743d9e79397f84fb2fee2
pviethung <pv2hung97@gmail.com>
2022-10-31 20:18:13 +0700

    init

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ echo > .nojekyll
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:pviethung/another-portfolio.git main:gh-pages
+git push -f git@github.com:pviethung/another-portfolio.git gh-pages
 
 cd -
