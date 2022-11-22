74fb85bf988652def52658406400edb37620dc97
pviethung <pv2hung97@gmail.com>
2022-11-22 22:20:24 +0700

    do stuff

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,14 +1,16 @@
 import { timeline } from '../data/timeline';
 
 const Timeline = () => {
+  const reversedTimeline = [...timeline].reverse();
+
   return (
     <div className="w-11/12 md:max-w-xl mx-auto mt-20">
       <h2 className="font-bold text-2xl mb-5 dark:text-white decoration-4 underline underline-offset-8 underth">
         Timeline
       </h2>
       <ul className="space-y-10 md:space-y-14 pl-4 relative before:absolute before:w-[1px] before:h-full before:bg-stone-200 before:left-0 before:top-0 pb-16">
-        {timeline.map((t, idx) => (
-          <li key={idx}>
+        {reversedTimeline.map((t) => (
+          <li key={t.title}>
             <div className="flex flex-col md:flex-row items-start md:items-center flex-wrap mb-3 relative before:absolute before:w-3 before:h-3 before:rounded-full before:bg-stone-200 before:-left-[22px]">
               <span className="py-1 px-2 mb-3 md:mb-0 font-[500] rounded-md text-white dark:text-stone-900 bg-stone-900 dark:bg-white">
                 {t.year}
